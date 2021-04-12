2f7c62f648798e35d882254bfcadae79c325ff2f
Alexmartinsena <48970060+Alexmartinsena@users.noreply.github.com>
2021-04-12 07:58:38 -0300

    Update Main.java

--- Projeto15/Hospital/src/Main.java
+++ Projeto15/Hospital/src/Main.java
@@ -1,50 +1,45 @@
-
 package hospital013;
-import java.util.Collection;
-import java.util.TreeMap;
+import java.util.*;
 
-public class Paciente implements IPaciente{
-	protected String id;
-	protected String diag; //diagnostico
-	protected TreeMap<String, IMedico> medicos;
+public class Main{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		Hospital hospital = new Hospital();
 
-	Paciente(String id, String diag){
-		this.id = id;
-		this.diag = diag;
-		this.medicos = new TreeMap<String, IMedico>();
+		while (true){
+			String line = scanner.nextLine();
+			System.out.println("$" + line);
+			String ui[] = line.split(" ");
+			
+			if(ui[0].equals("end"))
+				break;
+			
+			else if(ui[0].equals("addPac")){
+				for(int i = 1; i < ui.length;i++){
+					String dados[] = ui[i].split("-");
+					hospital.addPaciente(new Paciente(dados[0], dados[1]));
+				}
+			}
+            
+			else if (ui[0].equals("addMed")){
+				for(int i = 1; i < ui.length;i++){
+					String dados[] = ui[i].split("-");
+					hospital.addMedico(new Medico(dados[0], dados[1]));
+				}
+			}
+            
+			else if(ui[0].equals("show")){
+				System.out.print(hospital.showAll());
+			}
+            
+			else if(ui[0].equals("vinc")){
+				for(int i = 2; i < ui.length;i++){
+					hospital.vincular(ui[1], ui[i]);
+				}
+			}
+		}
+		
+		scanner.close();
+	
 	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void addMedico(IMedico medico) {
-		IMedico Xmedico = medicos.get(medico.getId());
-		if(Xmedico != null)
-			return;
-		medicos.put(medico.getId(), medico);
-		medico.addPaciente(this);
-	}
-
-	public void removerMedico(String idMedico) {
-		IMedico Xmedico = medicos.get(idMedico);
-		if(Xmedico == null)
-			return;
-		medicos.remove(idMedico);
-		Xmedico.removerPaciente(this.id);
-	}
-
-	public Collection<IMedico> getMedicos(){
-		return medicos.values();
-	}
-
-	public String getdiag(){
-		return diag;
-	}
-
-	public String toString(){
-		String ListMedicos = new String();
-		for(IMedico medico: getMedicos()) ListMedicos += medico.getId()+" ";
-		return "Pacientes: " + String.format(getId() + ":" + getdiag()) + " Medicos: [ " + ListMedicos + "] \n";
-	};
 }
