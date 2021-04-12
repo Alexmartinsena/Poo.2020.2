9e3042d72f22d8fa233dfd9dbccfed5865d95fed
Alexmartinsena <48970060+Alexmartinsena@users.noreply.github.com>
2021-04-12 07:57:06 -0300

    Update IBatePapense.java

--- Projeto15/Hospital/src/IBatePapense.java
+++ Projeto15/Hospital/src/IBatePapense.java
@@ -5,9 +5,11 @@ import java.util.List;
 interface IBatePapense {
 	String getId();
 
+	List<Mensagem> getDirect();
+
 	void addMessage(Mensagem msg);
 	
 	void sendMessage(Mensagem msg, IBatePapense batePapense);
 	
-	List<Mensagem> getInbox();
+	
 }
